c1481467cbd07332bbeba264eb890c66dce6767d
Gonzalo Montiel <gon.montiel@gmail.com>
2017-01-15 19:43:30 -0300

    Add department name to mock data

--- app/src/main/java/mont/gonzalo/phiuba/Course.java
+++ app/src/main/java/mont/gonzalo/phiuba/Course.java
@@ -9,7 +9,6 @@ import java.util.List;
 public class Course {
     public static final String KEY_COURSE_CODE = "key_course_code";
     public static final String KEY_COURSE_NAME = "key_course_name";
-    public static final String KEY_COURSE_LINK = "key_course_link";
 
     private String code;
     private String depCode;
@@ -21,10 +20,11 @@ public class Course {
     private Boolean required;
     private List<String> correlatives;
 
-    public Course(String name, String depCode, String code) {
+    public Course(String name, String depCode, String code, String depto) {
         this.name = name;
         this.code = code;
         this.depCode = depCode;
+        this.depto = depto;
     }
 
     public String getCode() {
@@ -108,26 +108,26 @@ public class Course {
     static List<Course> getSampleData() {
         if (sample_data == null) {
             List<Course> cc = new ArrayList<>();
-            cc.add(new Course("Fisica I A", "62", "62.01"));
-            cc.add(new Course("algebra II A", "61", "61.08"));
-            cc.add(new Course("Quimica", "63", "63.01"));
-            cc.add(new Course("Analisis Matematico II A", "61", "61.03"));
-            cc.add(new Course("Algoritmos y Programacion I", "75", "75.40"));
-            cc.add(new Course("Fisica II A", "62", "62.03"));
-            cc.add(new Course("Algoritmos y Programacion II", "75", "75.41"));
-            cc.add(new Course("Fisica III D", "62", "62.15"));
-            cc.add(new Course("Laboratorio", "66", "66.02"));
-            cc.add(new Course("Estructura del Computador", "66", "66.70"));
-            cc.add(new Course("Algoritmos y Programacion III", "75", "75.07"));
-            cc.add(new Course("Analisis Numerico I", "75", "75.12"));
-            cc.add(new Course("Probabilidad y Estadistica B", "61", "61.09"));
-            cc.add(new Course("Analisis Matematico III A", "61", "61.10"));
-            cc.add(new Course("Organizacion de Computadoras", "66", "66.20"));
-            cc.add(new Course("Organizacion de Datos", "75", "75.06"));
-            cc.add(new Course("Taller de Programacion I", "75", "75.42"));
-            cc.add(new Course("Estructura de las Organizaciones", "71", "71.12"));
-            cc.add(new Course("Modelos y Optimizacion I", "71", "71.14"));
-            cc.add(new Course("Sistemas Operativos", "75", "75.08"));
+            cc.add(new Course("Analisis Matematico II A", "61", "61.03", "Matemática"));
+            cc.add(new Course("Algoritmos y Programacion I", "75", "75.40", "Computación"));
+            cc.add(new Course("Algebra II A", "61", "61.08", "Matemática"));
+            cc.add(new Course("Fisica II A", "62", "62.03", "Física"));
+            cc.add(new Course("Algoritmos y Programacion II", "75", "75.41", "Computación"));
+            cc.add(new Course("Fisica III D", "62", "62.15", "Física"));
+            cc.add(new Course("Laboratorio", "66", "66.02", "Electrónica"));
+            cc.add(new Course("Estructura del Computador", "66", "66.70", "Electrónica"));
+            cc.add(new Course("Algoritmos y Programacion III", "75", "75.07", "Computación"));
+            cc.add(new Course("Analisis Numerico I", "75", "75.12", "Computación"));
+            cc.add(new Course("Probabilidad y Estadistica B", "61", "61.09", "Matemática"));
+            cc.add(new Course("Analisis Matematico III A", "61", "61.10", "Matemática"));
+            cc.add(new Course("Organizacion de Computadoras", "66", "66.20", "Electrónica"));
+            cc.add(new Course("Organizacion de Datos", "75", "75.06", "Computación"));
+            cc.add(new Course("Taller de Programacion I", "75", "75.42", "Computación"));
+            cc.add(new Course("Estructura de las Organizaciones", "71", "71.12", "Gestión"));
+            cc.add(new Course("Modelos y Optimizacion I", "71", "71.14", "Gestión"));
+            cc.add(new Course("Sistemas Operativos", "75", "75.08", "Computación"));
+            cc.add(new Course("Analisis de la Informacion", "75", "75.09", "Computación"));
+            cc.add(new Course("Tecnicas de Diseño", "75", "75.10", "Computación"));
             sample_data = cc;
         }
         return sample_data;
