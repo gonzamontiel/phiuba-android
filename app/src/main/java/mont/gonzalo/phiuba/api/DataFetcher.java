208bc3a9ef4bef18d0c3457d9d56720ba7d9eb22
Gonzalo Montiel <gon.montiel@gmail.com>
2017-04-09 11:45:12 -0300

    change log message

--- app/src/main/java/mont/gonzalo/phiuba/api/DataFetcher.java
+++ app/src/main/java/mont/gonzalo/phiuba/api/DataFetcher.java
@@ -51,7 +51,7 @@ public class DataFetcher extends Observable {
         if (instance == null) {
             instance = new DataFetcher(PROD_ROOT_URL);
         }
-        Log.d("asd", serverUrl);
+        Log.d("Connected to ", serverUrl);
         return instance;
     }
 
