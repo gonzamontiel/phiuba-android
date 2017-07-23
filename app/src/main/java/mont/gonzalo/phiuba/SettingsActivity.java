0b4adde9963bb9f9e535340c2661f1b3aeac682c
Gonzalo Montiel <gon.montiel@gmail.com>
2017-07-23 13:23:43 -0300

    Make settings back button work

--- app/src/main/java/mont/gonzalo/phiuba/SettingsActivity.java
+++ app/src/main/java/mont/gonzalo/phiuba/SettingsActivity.java
@@ -137,6 +137,16 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     /**
      * {@inheritDoc}
      */
